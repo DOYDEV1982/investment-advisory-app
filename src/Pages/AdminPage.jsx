07b3e06638ef61d601fbd153052d67366b2a270c
DOYDEV1982 <oladoyejelili53@yahoo.com>
2025-08-17 00:16:46 +0100

    investment app

--- src/Pages/AdminPage.jsx
+++ src/Pages/AdminPage.jsx
@@ -31,7 +31,7 @@ const AdminPage = () => {
         <h1>Dashboard</h1>
         <div className="admin-options">
         <Link to="/market-data" className="admin-card">
-          <h3>📈 Market Data</h3>
+          <h3>Market Data</h3>
           <p>View live exchange rates, currency trends, and investment suggestions.</p>
         </Link>
         <button onClick={handleLogout}>Logout</button>
@@ -42,24 +42,24 @@ const AdminPage = () => {
         <section className="admin-card">
           <h2>Market Insights</h2>
           <p>View global trends, currency fluctuations, and investment tips based on market stability.</p>
-          <button onClick={() => navigate('/marketdata')}>Go to Market Data</button>
+          <button onClick={() => navigate("/market-data")}>Go to Market Data</button>
         </section>
 
         <section className="admin-card">
           <h2>User Analytics</h2>
           <p>Track user interests, region-specific trends, and investment behavior.</p>
-          <button onClick={() => navigate('/User Analytics')}>Go to User Analytics Page</button>
+          <button onClick={() => navigate("/analytics")}>Go to User Analytics Page</button>
         </section>
         <section className="admin-card">
-          <h3>👤 User Management</h3>
+          <h3>User Management</h3>
           <p>Control access and manage user data securely.</p>
-          <button onClick={() => navigate('/User Management')}>Go to User Mangement Page</button>
+          <button onClick={() => navigate("/user-management")}>Go to User Mangement Page</button>
         </section>
 
         <section className="admin-card">
           <h2>Update Advisory Tips</h2>
           <p>Upload daily advice for stable or risky investment paths globally.</p>
-          <button onClick={() => navigate('/Advisory Tips')}>Go to Advisory Page</button>
+          <button onClick={() => navigate("/advisory")}>Go to Advisory Page</button>
         </section>
       </main>
       <Footer /> 
