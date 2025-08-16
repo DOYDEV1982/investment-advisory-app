66c81c531711b456414ed5cceeeeefd2afc2e740
DOYDEV1982 <oladoyejelili53@yahoo.com>
2025-08-16 19:06:15 +0100

    investment app

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,16 +1,17 @@
 import Navbar from '../components/Navbar'
-import Doydev from '../assets/doydev.png'
 import '../AboutPage.css'
 
 const AboutPage = () => {
   return (
       <>
     <Navbar />
-    <div className="AboutPage">
-      
+    <div className="AboutPage">  
     <div className="about">
       <h2>About This App</h2>
       <p>This platform helps investors explore markets and make informed, profitable decisions based on global data.</p>
+          <div className='button'>
+            <button className='btn-black'>Contact Us</button>
+          </div>
     </div>
     </div>
     </>
